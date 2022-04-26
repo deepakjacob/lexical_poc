55a82cd9f47c0c482fc5bc6c153ddc0dffbe875b
Deepak Jacob <deepak.jacob@gmail.com>
2022-04-26 12:47:01 -0400

    fix: autofocus and onchange

--- src/editor/editor.tsx
+++ src/editor/editor.tsx
@@ -1,45 +1,16 @@
-import { $getRoot, $getSelection, EditorState } from "lexical";
-import { useEffect } from "react";
-
 import LexicalComposer from "@lexical/react/LexicalComposer";
 import LexicalPlainTextPlugin from "@lexical/react/LexicalPlainTextPlugin";
 import LexicalContentEditable from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import LexicalOnChangePlugin from "@lexical/react/LexicalOnChangePlugin";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { TermNode } from "./TermNode";
 import { TermButton } from "./TermButton";
 import { TermPlugin } from "./TermPlugin";
 import { ActionsPlugin } from "./ActionsPlugin";
+import { AutoFocusPlugin } from "./AutoFocusPlugin";
+import { onChange } from "./onChange";
 
 const theme = {};
-
-// When the editor changes, you can get notified via the
-// LexicalOnChangePlugin!
-function onChange(editorState: EditorState) {
-  editorState.read(() => {
-    // Read the contents of the EditorState here.
-    const root = $getRoot();
-    const selection = $getSelection();
-
-    console.log(root, selection);
-  });
-}
-
-// Lexical React plugins are React components, which makes them
-// highly composable. Furthermore, you can lazy load plugins if
-// desired, so you don't pay the cost for plugins until you
-// actually use them.
-function MyCustomAutoFocusPlugin() {
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    // Focus the editor when the effect fires!
-    editor.focus();
-  }, [editor]);
-
-  return null;
-}
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -63,7 +34,7 @@ function Editor() {
         />
         <LexicalOnChangePlugin onChange={onChange} />
         <HistoryPlugin />
-        <MyCustomAutoFocusPlugin />
+        <AutoFocusPlugin />
         <TermPlugin />
         <TermButton />
         <ActionsPlugin />
